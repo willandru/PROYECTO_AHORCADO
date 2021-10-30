d934ac0b9828f6f9c04801cd7f7e693d0a2e4e1b
willandru <willandru@hotmail.com>
2021-10-30 14:11:52 -0400

    Im trying to add the Start gif Image to the first frame

--- V1_AHORCADO/Ahorcado/src/proyecto/ahorcado/view/UxAhorcado.java
+++ V1_AHORCADO/Ahorcado/src/proyecto/ahorcado/view/UxAhorcado.java
@@ -7,6 +7,11 @@ package proyecto.ahorcado.view;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Image;
+import java.io.IOException;
+import java.net.URL;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -27,19 +32,26 @@ public class UxAhorcado {
     
     JFrame frame;
     JLabel imageStart;
-    
+   
     
     public UxAhorcado() {
         // Creando ventana
         frame = new JFrame("Ahorcado V1");
-        frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(WIDTH_SCREEN, HEIGTH_SCREEN);
-        frame.setResizable(false);
+        frame.setResizable(true);
 
-     
+        imageStart = new JLabel();
+        imageStart.setVerticalAlignment(JLabel.CENTER);
+       
+
+        imageStart.setOpaque(true);
+        imageStart.setForeground(new Color(120, 90, 40));
+       // imageStart.setBackground(new Color(10, 70, 20));
+        this.addImagePanel("https://cdn.pling.com/img/4/5/8/1/c7d4bcbeac5f159189b860afb4bb9c7b92358a550db93e2a4cec03912b54001a6ddb.gif");
 
 
-
+        frame.add(imageStart);
         frame.setVisible(true);
 
     }
@@ -50,6 +62,20 @@ public class UxAhorcado {
             instance = new UxAhorcado();
         }
         return instance;
+    }
+    
+    public void addImagePanel(String path) {
+        Image image = null;
+        try {
+            URL url = new URL(path);
+            image = ImageIO.read(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Image newimg = image.getScaledInstance(WIDTH_SCREEN , HEIGTH_SCREEN/2 , java.awt.Image.SCALE_SMOOTH);
+        imageStart.setIcon(new ImageIcon(newimg));
+        
     }
     
 }
