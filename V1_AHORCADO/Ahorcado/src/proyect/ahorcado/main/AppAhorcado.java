4fcbef1f7995c23ad4c6d5abf644eca3f3cd438a
willandru <willandru@hotmail.com>
2021-11-03 23:30:51 -0400

    made some changes

--- V1_AHORCADO/Ahorcado/src/proyect/ahorcado/main/AppAhorcado.java
+++ V1_AHORCADO/Ahorcado/src/proyect/ahorcado/main/AppAhorcado.java
@@ -16,6 +16,7 @@ public class AppAhorcado {
     
     public static void main(String[] args) {
         ux= UxAhorcado.getInstance();
+        
     }
     
 }
--- V1_AHORCADO/Ahorcado/src/proyecto/ahorcado/view/UxAhorcado.java
+++ V1_AHORCADO/Ahorcado/src/proyecto/ahorcado/view/UxAhorcado.java
@@ -15,6 +15,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import proyecto.ahorcado.engine.StartImage;
 
 /**
  *
@@ -23,7 +24,7 @@ import javax.swing.JPanel;
 
 
 
-public class UxAhorcado {
+public class UxAhorcado extends JFrame {
     
      private static UxAhorcado instance;
     private static final int WIDTH_SCREEN = 400;
@@ -31,28 +32,21 @@ public class UxAhorcado {
    
     
     JFrame frame;
-    JLabel imageStart;
+    JPanel imageStart;
    
     
     public UxAhorcado() {
         // Creando ventana
-        frame = new JFrame("Ahorcado V1");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(WIDTH_SCREEN, HEIGTH_SCREEN);
-        frame.setResizable(true);
+        super("Ahorcado");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(WIDTH_SCREEN, HEIGTH_SCREEN);
+        this.setResizable(true);
 
-        imageStart = new JLabel();
-        imageStart.setVerticalAlignment(JLabel.CENTER);
-       
-
-        imageStart.setOpaque(true);
-        imageStart.setForeground(new Color(120, 90, 40));
-       // imageStart.setBackground(new Color(10, 70, 20));
-        this.addImagePanel("https://cdn.pling.com/img/4/5/8/1/c7d4bcbeac5f159189b860afb4bb9c7b92358a550db93e2a4cec03912b54001a6ddb.gif");
-
-
-        frame.add(imageStart);
-        frame.setVisible(true);
+        imageStart = new StartImage(this);
+        this.add(imageStart);
+        this.add(imageStart);
+        
+        this.setVisible(true);
 
     }
     
@@ -63,19 +57,11 @@ public class UxAhorcado {
         }
         return instance;
     }
-    
-    public void addImagePanel(String path) {
-        Image image = null;
-        try {
-            URL url = new URL(path);
-            image = ImageIO.read(url);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        Image newimg = image.getScaledInstance(WIDTH_SCREEN , HEIGTH_SCREEN/2 , java.awt.Image.SCALE_SMOOTH);
-        imageStart.setIcon(new ImageIcon(newimg));
-        
+     public void refreshUX() {
+        frame.setVisible(false);
+        frame.setVisible(true);
     }
+    
+   
     
 }
