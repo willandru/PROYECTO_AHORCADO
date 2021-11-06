9c935df914ec67b2c6f01ee3f5736b442b820aa3
willandru <willandru@hotmail.com>
2021-11-05 23:34:44 -0400

    FIX-ON-DEV

--- V1_AHORCADO/AHORCADO_V1/src/keyboard/KeyBoardEvents.java
+++ V1_AHORCADO/AHORCADO_V1/src/keyboard/KeyBoardEvents.java
@@ -5,10 +5,28 @@
  */
 package keyboard;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 /**
  *
  * @author kaliw
  */
-public class KeyBoardEvents {
+public class KeyBoardEvents implements KeyListener{
+
+    @Override
+    public void keyTyped(KeyEvent ke) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void keyPressed(KeyEvent ke) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void keyReleased(KeyEvent ke) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
